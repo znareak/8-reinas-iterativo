8fb2e8dba9a5d396de9ab6fd3d3c1570a2e68045
znareak <libardojesusrengifo129@gmail.com>
2020-12-01 09:26:59 -0430

    remove comments

--- src/pkg8/reinas/MainProgram.java
+++ src/pkg8/reinas/MainProgram.java
@@ -1,9 +1,12 @@
 package pkg8.reinas;
+
 import pkg8.reinas.Stack.piladinamica;
 import static pkg8.reinas.Helpers.logLn;
 
 public class MainProgram {
+
     final private static piladinamica<Queen> queensPlacedList = new piladinamica<Queen>();
+    int row = 0, column = 0;
     private static Queen queen = new Queen(0, 0);
     private static BoardStack board;
     private static int queensPlaced = 0, totalSolutions = 0, queens;
@@ -11,14 +14,17 @@ public class MainProgram {
     public MainProgram(int q) {
         queens = q;
         board = new BoardStack(q);
-        solutions(0, 0);
+        /*while(queen != null)
+            solutions();*/
+        solutions();
     }
 
-    private void solutions(int row, int column) {
+    private void solutions() {
         if (queens == 2 || queens == 3) {
             logLn("Para este número de reinas no hay soluciones.");
             return;
         }
+        
         if (queensPlaced == queens) {
             totalSolutions++;
             board.printBoard();
@@ -43,51 +49,8 @@ public class MainProgram {
         } else {
             queen.addColumn();
         }
-        solutions(queen.getRow(), queen.getColumn());
+        row = queen.getRow();
+        column = queen.getColumn();
+        solutions();
     }
-
-    /*public MainProgram(int queens) {
-        this.queens = queens;
-        putQueens(0, 0, new Queen(0, 0), new BoardStack(queens));
-    }*/
- /*private void putQueens(int row, int column, Queen queen, BoardStack board) {
-        if (queens == 2 || queens == 3) {
-            logLn("Para este número de reinas no hay soluciones.");
-            return;
-        }
-        if (queensPlaced == queens) {
-            totalSolutions++;
-            if (queen.getColumn() >= 8) {
-                board.fillBoard();
-                queensPlaced = 0;
-                while (queensPlacedList.top() != null) {
-                    queen = queensPlacedList.pop();
-                }
-                queen.addColumn();
-            }
-            board.printBoard();
-            logLn("Reinas colocadas: " + queensPlaced + "\nSolucion nro. " + totalSolutions + "\n\n");
-        }
-
-        if ((row >= queens) || (column >= queens)) {
-
-            queen = queensPlacedList.pop();
-            if (queen == null) {
-                return;
-            }
-            board.removeQueen(queen);
-            queensPlaced--;
-            queen.addColumn();
-
-        } else if (board.isGoodChessBox(queen)) {
-            board.addQueen(queen);
-            queensPlacedList.push(new Queen(row, column));
-            queensPlaced++;
-            queen.setCoords(row + 1, 0);
-
-        } else {
-            queen.addColumn();
-        }
-        putQueens(queen.getRow(), queen.getColumn(), queen, board);
-    }*/
 }
